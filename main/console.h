ee3aaaf70efa85068b795625e3e86e088f3f2444
cbq <bqchen@car2p.com>
2021-01-23 21:57:00 +0800

    添加console组件

---
+++ main/console.h
@@ -0,0 +1,7 @@
+#ifndef _CONSOLE_H_
+#define _COMSOLE_H_
+
+void console_task_init(void);
+
+
+#endif
--- main/main.c
+++ main/main.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "console.h"
 
 
 void app_main(void)
 {
+    console_task_init();
     int i = 0;
     while (1) {
-        printf("[%d] Hello world!\n", i);
+        //printf("[%d] Hello world!\n", i);
         i++;
         vTaskDelay(5000 / portTICK_PERIOD_MS);
     }
